9a1c7a9bdcbc35791936a92a178a060c0e807f64
CelioSoares996 <franciscocelio936@gmail.com>
2024-06-16 15:33:43 -0300

    first commit

---
+++ src/main/java/com/sistemacadastro/sistemacadastro/SistemacadastroApplication.java
@@ -0,0 +1,13 @@
+package com.sistemacadastro.sistemacadastro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemacadastroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemacadastroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sistemacadastro/sistemacadastro/SistemacadastroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistemacadastro.sistemacadastro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemacadastroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
